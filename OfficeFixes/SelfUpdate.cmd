d2bdbfb072e1b9be85cb07e94d054cbcc4d684a2
Just a Dark Dinosaur <97694728+DarkDinosaurEx@users.noreply.github.com>
2022-05-10 23:20:36 +0300

    Add files via upload

--- OfficeFixes/SelfUpdate.cmd
+++ OfficeFixes/SelfUpdate.cmd
@@ -5,7 +5,7 @@
 	
 	rem Run as Admin with native shell, any path, params, loop guard, minimal i/o, by AveYo
 	>nul reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\" &call \"%%2\" %%3" & set "_= %*"
-	>nul fltmc || if "%f0%" neq "%~f0" ( cd.>"%tmp%\runas.Admin" & start "%~n0" /high "%tmp%\runas.Admin" "%~f0" "%_:"=""%" &exit /b )
+	>nul fltmc || if "%f0%" neq "%~f0" ( cd.>"%tmp%\runas.Admin" & start "%~n0" /high /min "%tmp%\runas.Admin" "%~f0" "%_:"=""%" &exit /b )
 
 	timeout 2
 	taskkill /PID %~1
